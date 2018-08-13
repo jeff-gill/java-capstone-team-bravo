5d4cc1f9b82afc78b2cd99c795a091f001efabb3
Gordy <gordy.smith1@gmail.com>
2018-08-13 19:53:41 -0400

    copy file to data.sql

--- database/data.sql
+++ database/data.sql
@@ -7,3 +7,23 @@ BEGIN;
 -- INSERT statements go here
 
 COMMIT;
+
+drop table user_info;
+
+create table user_info ( 
+        user_name varchar(35) not null,
+        password varchar(20) not null,
+        first_name varchar (35) not null,
+        last_name varchar (35) not null,
+        bio varchar (360) not null,
+        ratings int, 
+        is_sensei boolean not null,
+
+        constraint pk_user_name primary key (user_name)
+
+);
+
+select * from user_info
+where user_name = 'lordgenius'
+
+insert into user_info (user_name, password, first_name, last_name, bio, ratings, is_sensei) values ('lordgenius', '1234', 'Gordy', 'Smith', 'I am a genius and a lord', 5, true);
