45735778bec4d08b817a8d92826528b9903efd89
ksparks <kaitiesparks@gmail.com>
2018-08-16 21:01:53 -0400

    database data renamed for subjects

--- database/schema.sql
+++ database/schema.sql
@@ -9,10 +9,7 @@ DROP TABLE IF EXISTS app_user;
 drop table if exists user_info;
 drop table if exists subjects;
 drop table if exists user_subjects;
-drop table if exists class;
-drop table if exists user_class;
 drop sequence if exists seq_class_id;
-drop sequence if exists seq_subject_id;
 
 CREATE TABLE app_user (
   id SERIAL PRIMARY KEY,
@@ -25,7 +22,6 @@ CREATE TABLE app_user (
 create table user_info ( 
     user_name varchar(35) not null UNIQUE,
     password varchar(20) not null,
-    --salt varchar(255) NOT NULL,
     first_name varchar (35) not null,
     last_name varchar (35) not null,
     bio varchar (360) not null,
@@ -52,14 +48,14 @@ create table subjects (
 	available_slots integer not null,
 	description varchar(360) not null,
 	
-	constraint pk_class_id primary key (class_id),
+	constraint pk_class_id primary key (class_id)
 );
 
-create table user_class (
+create table user_subjects (
 	class_id integer not null,
 	user_name varchar(35) not null,
 	
-	constraint fk_class_id foreign key (class_id) references class (class_id) on delete cascade,
+	constraint fk_class_id foreign key (class_id) references subjects (class_id) on delete cascade,
 	constraint fk_user_name foreign key (user_name) references user_info (user_name) on delete cascade
 );
 
