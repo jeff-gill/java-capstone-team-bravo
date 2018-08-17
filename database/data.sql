2502f044e76406ee16f78bfbb3fcef60d902ad68
ksparks <kaitiesparks@gmail.com>
2018-08-16 21:03:46 -0400

    database data renamed for subjects

--- database/data.sql
+++ database/data.sql
@@ -4,8 +4,8 @@
 
 insert into user_info (user_name, password, first_name, last_name, bio, ratings, is_sensei, email, phone, profile_image, interests) values ('lordgenius', '1234', 'Gordy', 'Smith', 'Lorem ipsum dolor amet mlkshk venmo semiotics kitsch, fixie cliche ramps biodiesel iceland farm-to-table pitchfork la croix pinterest XOXO. Mustache VHS taiyaki green juice, small batch pok pok blue bottle pug aesthetic tilde trust fund locavore vape.', 5, true, 'gordy@swordfighting.com', '614-555-3456', 'sensei', 'zombies');
 
-insert into subject (subject_name, location, event_date, event_start_time, event_end_time, cost, available_slots, description) values ('Sword Fighting', 'Goodale Park', '10/25/2018', '10:00', '11:00', 25.0, 4, 'Learn to Slash Zombies');
+insert into subjects (subject_name, location, event_date, event_start_time, event_end_time, cost, available_slots, description) values ('Sword Fighting', 'Goodale Park', '10/25/2018', '10:00', '11:00', 25.0, 4, 'Learn to Slash Zombies');
 
-insert into user_class (class_id, user_name) values (1, 'lordgenius');
+insert into user_subjects (class_id, user_name) values (1, 'lordgenius');
 
 insert into user_info (user_name, password, first_name, last_name, bio, ratings, is_sensei, email, phone, profile_image) values('YoungSon', 1234, 'Jon', 'Snow', 'Lorem ipsum dolor amet migas retro shabby chic, hot chicken vice hell of health goth before they sold out irony. Retro pop-up raclette cornhole YOLO lomo. Gluten-free waistcoat la croix, skateboard organic pabst dreamcatcher pok pok adaptogen direct trade wayfarers art party', 4, false, 'jonSnow@got.com', '419-706-0235', 'gh');
